e31969652f4c86125bcce1790968a2b15f4b9f73
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2017-06-13 14:07:57 +0530

    Added port config for deployment

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ var path = require('path');
 app.use(express.static('./app'));
 
 var IP = "localhost";
-var port = 3000;
+var port = process.env.PORT || 3000
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-app.listen(port, IP, function() {
+app.listen(port, function() {
     console.log(" Listening at port : ", port);
 });
